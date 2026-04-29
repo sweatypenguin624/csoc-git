db53342e94583d7a3562be3db168d3702a9417d4
Trisha. D <trishadayanand54@gmail.com>
2026-04-29 14:24:15 +0530

    Implement marks input and grading system
    Added functionality to input marks and evaluate pass or fail along with grades based on average marks.

--- TrishaD.py
+++ TrishaD.py
@@ -1,3 +1,5 @@
+# ASSIGNMENT - 1
+
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
@@ -29,4 +31,30 @@ if extra_cheese == "Y":
 
 print(f"Your final bill is: Rs{bill}.")
 
-Name : Trisha D
+# Assignment -2 :
+# Input Marks from user using loop ; Evaluate Pass or Fail / Grades using conditional statements
+
+marks=[]
+s = int(input("Enter number of subjects :"))
+for i in range(s):
+    mark = float(input(f"Enter marks for subject {i+1} :"))
+    marks.append(mark)
+avg = sum(marks) / s
+print(f"Average marks : {avg}")
+if any(mark < 40 for mark in marks) :
+    print("Result : Fail")
+    print("Grade : F")
+else :
+    print("Result : Pass")
+    if avg >=90:
+        print("Grade : A+")
+    elif avg>=80 :
+        print("Grade : A")
+    elif avg >=70:
+        print("Grade : B")
+    elif avg >=60:
+        print("Grade : C")
+    elif avg >=50:
+        print("Grade : D")
+    else :
+        print("Grade : E")
