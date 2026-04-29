201567f8e725e115964a9011e18c8b958290e7da
Trisha. D <trishadayanand54@gmail.com>
2026-04-29 14:25:22 +0530

    Update TrishaD.py
    Removed todo comments and added input for marks evaluation.

--- TrishaD.py
+++ TrishaD.py
@@ -5,8 +5,6 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
-# todo: work out how much they need to pay based on their size choice.
-
 bill = 0
 
 if size == "S":
@@ -18,21 +16,19 @@ elif size == "L":
 else:
     print("You have chosen an invalid size.")
 
-# todo: work out how much to add to their bill based on their pepperoni choice.
 if pepperoni == "Y":
     if size == "S":
         bill += 80
     else:
         bill += 119
 
-# todo: work out their final amount based on whether if they want extra cheese.
 if extra_cheese == "Y":
     bill += 50
 
 print(f"Your final bill is: Rs{bill}.")
 
 # Assignment -2 :
-# Input Marks from user using loop ; Evaluate Pass or Fail / Grades using conditional statements
+# Input Marks from user using loop ; Evaluate Pass or Fail and Grades using conditional statements
 
 marks=[]
 s = int(input("Enter number of subjects :"))
